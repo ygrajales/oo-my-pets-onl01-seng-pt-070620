76582d60ff41de3433d66eaeba379808c9559a43
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-08-04 03:00:24 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -33,30 +33,49 @@ class Owner
   
   def cats
     Cat.all.select{|cat| cat.owner == self }
-    end
-     def dogs
+  end
+     
+     
+  def dogs
     Dog.all.select{|dog| dog.owner == self }
-    end
+  end
+  
+  
   def buy_cat(name)
     Cat.new(name, self)
   end
+  
+  
   def buy_dog(name)
     Dog.new(name, self)
   end
+  
+  
   def walk_dogs
     self.dogs.each {|dog| dog.mood = "happy"}
   end
+  
+  
   def feed_cats
     self.cats.each {|cat| cat.mood = "happy"}
   end
+  
+  
   def sell_pets
+    
     pets = self.dogs + self.cats
+    
     pets.each do |pets|
       pets.mood = "nervous"
       pets.owner = nil
     end
+    
   end
+  
+  
   def list_pets
     "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
   end
+  
+  
 end
